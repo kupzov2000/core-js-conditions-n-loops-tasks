0485d766601d0f6e8d559994c282211ca934b849
Evgeny Kuptsov <kupzov200@mail.ru>
2024-01-04 01:13:01 +0400

    feat: add a solution to the task of finding a polydrome

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -251,8 +251,15 @@ function convertNumberToString(numberStr) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  const count = Math.ceil(str.length / 2);
+
+  for (let i = 0; i < count; i += 1) {
+    if (str[i] !== str[str.length - i - 1]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
